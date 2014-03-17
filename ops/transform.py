c61e8a01d9d38984f205610a2b781efb35b8c647
Josh Roppo <joshroppo@gmail.com>
2014-03-16 17:22:07 -0700

    Working on getting FreqDist ops running.

--- ops/transform.py
+++ ops/transform.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, unicode_literals
 
 from PyPDF2 import PdfFileReader
+from nltk import FreqDist
 from nltk.corpus import stopwords
 
 def pdf_text(pdf_file):
@@ -12,3 +13,8 @@ def pdf_text(pdf_file):
 def stop_words(text):
     stop = stopwords.words('english')
     return ' '.join([ w for w in text.split() if w.lower() not in stop ])
+
+def freq_dist_count(text):
+    fdist = FreqDist(text) 
+    return [ (v, k) for k,v in fdist.iteritems() ]
+
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -5,7 +5,7 @@ import os
 from sys import stderr
 import unittest
 
-from ops.transform import stop_words
+from ops.transform import stop_words, freq_dist_count
 
 base_resources = '{}/tests/resources/'.format(os.getcwd())
 
@@ -21,4 +21,20 @@ class TestTransformOps(unittest.TestCase):
             #print(stopped, file=stderr)
             self.assertGreater(len(stopped.split()), 50)
             self.assertNotEqual(text, stopped)
+
+    def test_stop_words_decode_fail(self):
+        with open('{}{}'.format(base_resources, 'lebowskiIpsum'), 'r')\
+        as f:
+            text = f.read()
+            self.assertRaises(UnicodeDecodeError, stop_words, text)
+
+    def test_freq_dist(self):
+        with open('{}{}'.format(base_resources, 'lebowskiIpsum'), 'r')\
+        as f:
+            text = f.read().decode('utf-8')
+            #print(text, file=stderr)
+            stopped = stop_words(text)  
+
+            freq_dist = freq_dist_count(stopped.split()) 
+            print(pformat(freq_dist), file=stderr)
 
