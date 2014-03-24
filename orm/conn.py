c78f6f459d852556d944023cd7296a1419dff61a
Josh Roppo <joshroppo@gmail.com>
2014-03-23 21:18:55 -0700

    Setting up ORM connection to Postgres

---
+++ orm/conn.py
@@ -0,0 +1,26 @@
+from __future__ import print_function, unicode_literals
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from tasks.auth import auth
+
+print(auth)
+conn_str = 'postgresql+psycopg2://{user}:{password}@{host}/{database}'
+
+def connect_engine():
+    x = conn_str.format(user=auth['user'],\
+                    password=auth['password'], host='localhost',\
+                    database=auth['database'])
+    print(x)
+    engine = create_engine(x)
+    print(engine)
+    return engine
+
+def declare_base():
+    Base = declarative_base() 
+    return Base
+
+def make_session(engine):
+    return sessionmaker(bind=engine)
+
