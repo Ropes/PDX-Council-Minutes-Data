cf0803573546430e62769720275ad7f14fbd135f
Josh Roppo <joshroppo@gmail.com>
2014-05-29 16:19:13 -0700

    Attempting to write meaninful SQL to decipher data

---
+++ sql/TokensFollowingPortland.sql
@@ -0,0 +1,15 @@
+﻿SELECT 
+	T.token,
+	T.count,
+	TL.dateid, 
+	TL.source, 
+	(SELECT Ts.token from "Token" Ts WHERE Ts.tokenid = TL.target),
+	TL.target, 
+	TL.distance, 
+	TL.index 
+  FROM 
+	"Token" T INNER JOIN "TokenLinks" TL
+  ON
+	T.tokenid = TL.source
+  WHERE T.token =  'Portland' AND TL.distance < 4 AND TL.distance >=0
+LIMIT 10000;
