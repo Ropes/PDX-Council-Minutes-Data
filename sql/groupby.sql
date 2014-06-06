76e2b36546c0db24406ce89295896b8a6147b916
Josh Roppo <joshroppo@gmail.com>
2014-06-06 14:32:21 -0700

    Fixing up templates and starting loading data task

---
+++ sql/groupby.sql
@@ -0,0 +1,13 @@
+﻿
+SELECT 
+T.token, 
+count(*) as C
+
+FROM "TokenLinks" TL JOIN "Token" T
+ON T.tokenid = TL.target
+WHERE TL.source = 
+(SELECT tokenid FROM "Token" T WHERE T.token = 'Portland')
+AND TL.distance > -4 AND TL.distance < 28 and T.dateid=8
+
+GROUP BY T.token HAVING count(*) > 2
+ORDER BY C DESC
--- tasks/loading.py
+++ tasks/loading.py
@@ -4,6 +4,8 @@ import ConfigParser
 from os import getenv
 
 import luigi
+from tasks.transform import StopListText
+from orm.transform import process_text
 
 from . import auth
 
@@ -11,10 +13,20 @@ class LoadText(luigi.Task):
     date = luigi.Parameter()
     
     def requires(self):
-        pass
+        return StopListText(self.date)
 
     def output(self):
         pass
 
     def run(self):
+        with self.input().open('r') as I:
+            raw_text = I.read()
 
+            tokens_counted = create_tokens(raw_text)
+
+            
+            #TODO: Generate token data
+
+            #TODO: Create token_links
+
+            
