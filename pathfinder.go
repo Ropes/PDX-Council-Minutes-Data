8e8847b7ca72378e6804edfbac1652ce003ce02d
Josh Roppo <joshroppo@gmail.com>
2014-09-06 14:46:27 -0700

    Easy format for creating time.Time and comments

--- pathfinder.go
+++ pathfinder.go
@@ -6,6 +6,20 @@ import (
 	"time"
 )
 
+//Returns a time.Date object based on the Year, Month, and Date only
+func EasyDate(year, month, date int) time.Time {
+	return time.Date(year, time.Month(month), date, 0, 0, 0, 0, time.UTC)
+}
+
+//Returns a path from the projects main directory to a meeting date's
+//data directory. Formatted as follows
+//project_dir/data/
+//    2014/
+//        /<month/
+//            /<day of filing>/
+//                minutes.pdf
+//                log?
+//                processed_data_files
 func MinutesDataDir(date time.Time) string {
 	y, m, d := date.Date()
 	return filepath.Join("data", strconv.Itoa(y),
